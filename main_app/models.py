e0b192bc0e98005e2532e9bba6e9af5839971659
jesse-peraza <perazajesse@yahoo.com>
2023-09-19 23:08:09 -0700

    complete functionality

--- main_app/models.py
+++ main_app/models.py
@@ -1,10 +1,29 @@
 from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
 
 POPTRENDS = (
     ('INC', 'Increading'),
     ('DEC', 'Decreasing'),
     ('UNK', 'Unknown')
 )
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+class Sponsor(models.Model):
+  name = models.CharField(max_length=50)
+  age = models.IntegerField()
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('sponsor_detail', kwargs={'pk': self.id})
 
 class Finch(models.Model):
 
@@ -17,7 +36,31 @@ class Finch(models.Model):
         choices=POPTRENDS,
         default=POPTRENDS[2][0]
     )
-
+    sponsors = models.ManyToManyField(Sponsor)
 
     def __str__(self):
         return f'{self.species} ({self.id})'
+    
+    def __str__ (self):
+        return f'{self.get_population_trend_display()}'
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+    
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+
+class Feeding(models.Model):
+    date = models.DateField('Feeding Date')
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    finch = models.ForeignKey(
+        Finch,
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}" 
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from .models import Finch
+from .models import Finch, Sponsor
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView,  UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .forms import FeedingForm
 
 def home(request):
   return render(request, 'home.html')
@@ -15,4 +18,53 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
    finch = Finch.objects.get(id=finch_id)
-   return render(request, 'finches/detail.html', { 'finch': finch })
+   id_list = finch.sponsors.all().values_list('id')
+   sponsors_finch_doesnt_have = Sponsor.objects.exclude(id__in=id_list)
+   feeding_form = FeedingForm()
+   return render(request, 'finches/detail.html', {
+      'finch': finch,
+      'feeding_form': feeding_form,
+      'sponsors': sponsors_finch_doesnt_have
+    })
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
+
+  def form_valid(self, form):
+    form.instance.user = self.request.user
+    return super().form_valid(form) 
+  
+class FinchUpdate(UpdateView):
+  model = Finch
+  fields = ['population', 'habitat', 'description', 'population_trend']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
+
+class SponsorList(ListView):
+  model = Sponsor
+
+class SponsorDetail(DetailView):
+  model = Sponsor
+
+class SponsorCreate(CreateView):
+  model = Sponsor
+  fields = '__all__'
+
+def assoc_sponsor(request, finch_id, sponsor_id):
+  Finch.objects.get(id=finch_id).sponsors.add(sponsor_id)
+  return redirect('detail', finch_id=finch_id)
+
+def unassoc_sponsor(request, finch_id, sponsor_id):
+  Finch.objects.get(id=finch_id).sponsors.remove(sponsor_id)
+  return redirect('detail', finch_id=finch_id)
+
+def add_feeding(request, finch_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
